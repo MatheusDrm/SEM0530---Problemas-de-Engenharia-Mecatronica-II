54366e71de6926f126484233174b967598ba14c3
Matheus Della Rocca Martins <84210251+MatheusDrm@users.noreply.github.com>
2022-08-05 17:53:03 -0300

    Add files via upload

---
+++ Prática3/kef.m
@@ -0,0 +1,12 @@
+function f = kef(u)
+    % Dados fornecidos pelo problema
+    L = 0.5; %(m)
+    d = 0.2; %(m)
+    h = sqrt ( power (L, 2) - power (d, 2)); %(m)
+    k = 10000; %(N/m)
+    g = 9.81; % (m/s^2)
+    m = 211; %(Kg)
+
+    %Eq. obtida:
+    f = (2 .* k .* (L - sqrt(power(d,2) + power ((h-u), 2))) .* ((h-u) ./ (sqrt(power(d,2) + power ((h-u), 2))))) ./ u;
+end
